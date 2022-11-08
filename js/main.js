9e6cda5220a058532b6762d5cd5164d3f82319ee
Pablo Nunez <polnunez@outlook.com>
2022-11-08 16:08:29 -0300

    añadida hora de creación de tarea

--- js/main.js
+++ js/main.js
@@ -2,32 +2,44 @@ const btnAddTask = document.querySelector('#btnAddTask');
 const taskTitle = document.querySelector('#taskTitle');
 const taskBody = document.querySelector('#taskBody');
 const tasksContainer = document.querySelector('#tasksContainer');
-const btnDeleteTask = document.querySelectorAll('#btnDeleteTask');
+const spanErrorMessage = document.querySelector('.spanErrorMessage');
+
 const totalTasksElement = document.querySelector('#totalTasks');
 let totalTasks = 0;
 const pendingTasksElement = document.querySelector('#pendingTasks');
 const finishedTasksElement = document.querySelector('#finishedTasks');
 let finishedTasks = 0;
+
 let month = new Date().getMonth() + 1;
 let day = new Date().getDate();
-document.getElementById("spanCurrentDate").innerHTML = `${day}/${month}`;
+let hours = new Date().getHours();
+let minutes = new Date().getMinutes();
+document.getElementById("spanCurrentDate").innerHTML = `Hoy, ${day}/${month}`;
 
 const tasks = [];
 
-const addTask = (e) => {
-    e.preventDefault();
+const addTask = () => {
+    if (taskTitle.value === '' && taskBody.value === '') {
+        spanErrorMessage.innerHTML = 'Los campos no deben estar vacíos.';
+        return;
+    } else if (taskTitle.value === '') {
+        spanErrorMessage.innerHTML = 'La tarea debe tener al menos el título.';
+        return;
+    }
     const task = {
         title: taskTitle.value,
         body: taskBody.value
     }
-    tasks.push(task);
+    tasks.unshift(task);
     renderTasks();
     taskTitle.value = '';
     taskBody.value = '';
+
     localStorage.setItem('tasks', JSON.stringify(tasks));
-    pendingTasksElement.innerHTML = tasks.length;
+
+    pendingTasksElement.innerHTML = tasks.length.toString();
     totalTasks++;
-    totalTasksElement.innerHTML = totalTasks;
+    totalTasksElement.innerHTML = totalTasks.toString();
 }
 
 const renderTasks = () => {
@@ -38,7 +50,8 @@ const renderTasks = () => {
                 <div class="card-body">
                     <h5 class="card-title">${task.title}</h5>
                     <p class="card-text">${task.body}</p>
-                    <button id="btnDeleteTask" class="btn btn-danger" onclick="finishTask(${index})">Finalizar</button>
+                    <span class="form-text">hoy a las ${hours} horas y ${minutes} minutos</span>
+                    <button class="btn btn-danger" onclick="finishTask(${index})">Finalizar</button>
                 </div>
             </div>
         `;
@@ -50,8 +63,8 @@ const finishTask = (index) => {
     renderTasks();
     localStorage.setItem('tasks', JSON.stringify(tasks));
     finishedTasks++;
-    finishedTasksElement.innerHTML = finishedTasks;
-    pendingTasksElement.innerHTML = tasks.length;
+    finishedTasksElement.innerHTML = finishedTasks.toString();
+    pendingTasksElement.innerHTML = tasks.length.toString();
 }
 
 btnAddTask.addEventListener('click', addTask);
